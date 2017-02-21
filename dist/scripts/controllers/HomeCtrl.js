28d9040bc4f6c2459b560d5b3c516514d1e884dc
bl2i4n <bl2i4n@yahoo.com>
2017-02-20 23:48:27 -0800

    getting checkpoint-4 up to date

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -3,20 +3,44 @@
     function HomeCtrl($scope, roomFactory, $uibModal){
         $scope.roomsList = roomFactory.all;
         
-        //change scope to another function
-        $scope.createRoomModal = function(){
-            //to open a popup window use the $uiModal.open method call
-            $uibModal.open({
-                //what do you want the modal to open up
-                //link to html for popup dialog
-                animation: true,
-                templateUrl:'/templates/modal.html',
-                controller: 'CreateRoomCtrl'
-            })
-     
-        };
+//        //change scope to another function
+//        $scope.createRoomModal = function(){
+//            //to open a popup window use the $uiModal.open method call
+//            $uibModal.open({
+//                //what do you want the modal to open up
+//                //link to html for popup dialog
+//                animation: true,
+//                templateUrl:'/templates/modal.html',
+//                controller: 'CreateRoomCtrl'
+//            })
+//     
+//        };
+//    }
+        
+        this.openModal = function(){
+            var modalInstance = $uibModal.open({
+                templateUrl: '/templates/modal.html',
+                //function for modal    
+                controller: function($scope, $uibModalInstance){
+                    $scope.newRoom = {$value: ''};
+                    
+                    $scope.cancel = function() {
+                        $uibModalInstance.dismiss('cancel');
+                    };
+                
+                    $scope.create = function(){
+                        $uibModalInstance.close($scope.newRoom);
+                    };
+                },
+                size: 'md',
+            });
             
-    }
+            modalInstance.result.then(function(data){
+               roomFactory.createRoom(data); 
+            });
+        
+        };
+    };
  
     angular
         .module('blocChat')
