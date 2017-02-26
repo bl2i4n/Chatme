efc5a264be17357f9e431e0d2b173797527dd779
bl2i4n <bl2i4n@yahoo.com>
2017-02-25 20:00:44 -0800

    started checkpoint 6 on master

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -3,18 +3,22 @@
         var ref = firebase.database().ref().child("messages");
 
         var messages = $firebaseArray(ref);
-        
+
         return {
             getByRoomId: function (roomId){
-            
+
             //filter messages by their room ID
                 return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+            },
+
+            send: function(newMessage){
+              //send method logic for a new message
             }
         };
     }
-    
+
     angular
         .module('blocChat')
         .factory('Message', ['$firebaseArray', Message]);
-    
+
 })();
--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,27 +1,26 @@
 (function(){
-    
+
     function HomeCtrl($scope, roomFactory, $uibModal, Message){
         $scope.roomsList = roomFactory.all;
 
         //load without a room selected, used to hold current room
         this.currentRoom = null;
-        
+
         //states whether room name is shown or not
         this.currentRoomShowing = false;
-        
+
         this.messages = null;
-        
-        
+
+
         //sets the current room to the one clicked
         this.setCurrentChatRoom = function(clickedRoom){
-//            debugger;
             this.currentRoom = clickedRoom;
             this.messages = Message.getByRoomId(this.currentRoom.$id);
             console.log(this.currentRoom.$id);
             console.log(this.messages);
         };
-            
-        
+
+
 //        tried using another controller to use uibmodal
 //        $scope.createRoomModal = function(){
 //            //to open a popup window use the $uiModal.open method call
@@ -32,38 +31,38 @@
 //                templateUrl:'/templates/modal.html',
 //                controller: 'CreateRoomCtrl'
 //            })
-//     
+//
 //        };
 //    }
-        
+
         //method for Home Controller to open modal, used Travis Rodger's method
         this.openModal = function(){
             var modalInstance = $uibModal.open({
                 templateUrl: '/templates/modal.html',
-                //function for modal    
+                //function for modal
                 controller: function($scope, $uibModalInstance){
                     $scope.newRoom = {$value: ''};
-                    
+
                     $scope.cancel = function() {
                         $uibModalInstance.dismiss('cancel');
                     };
-                
+
                     $scope.create = function(){
                         $uibModalInstance.close($scope.newRoom);
                     };
                 },
                 size: 'md',
             });
-            
+
             modalInstance.result.then(function(data){
-               roomFactory.createRoom(data); 
+               roomFactory.createRoom(data);
             });
-        
+
         };
     };
- 
+
     angular
         .module('blocChat')
         .controller('HomeCtrl', ['$scope', 'roomFactory', '$uibModal', 'Message', HomeCtrl]);
- 
+
  })();
